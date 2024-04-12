94cddf70cbfce454bb4d4d5540f24259c27da75d
benimustiko <beni.mustiko.a@gmail.com>
2024-04-12 22:58:13 +0700

    setup gh-pages

---
+++ src/context/Context.jsx
@@ -0,0 +1,71 @@
+import { createContext, useState } from "react";
+import runChat from "../config/gemini";
+
+export const Context = createContext();
+
+export const ContextProvider = (props) => {
+  const [input, setInput] = useState("");
+  const [recentPrompt, setRecentPrompt] = useState("");
+  const [previousPrompt, setPreviousPrompt] = useState([]);
+  const [showResult, setShowResult] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [resultData, setResultData] = useState("");
+
+  const delayPara = (index, nextWord) => {
+    setTimeout(() => {
+      setResultData((prev) => prev + nextWord);
+    }, 75 * index);
+  };
+
+  const newChat = () => {
+    setLoading(false);
+    setShowResult(false);
+  };
+
+  const onSent = async (prompt) => {
+    setResultData("");
+    setLoading(true);
+    setShowResult(true);
+    let response;
+    if (prompt !== undefined) {
+      response = await runChat(prompt);
+      setRecentPrompt(prompt);
+    } else {
+      setPreviousPrompt((prev) => [...prev, input]);
+      setRecentPrompt(input);
+      response = await runChat(input);
+    }
+
+    let newResponse = response.replace(/\*\*(.*?)\*\*/g, "<b>$1</b>");
+    let newResponse2 = newResponse.replace(/\*/g, "<br/>");
+    let newResponseArray = newResponse2.split(" ");
+    for (let i = 0; i < newResponseArray.length; i++) {
+      const nextWord = newResponseArray[i];
+      delayPara(i, nextWord + " ");
+    }
+
+    setLoading(false);
+    setInput("");
+  };
+
+  const contextValue = {
+    input,
+    setInput,
+    recentPrompt,
+    setRecentPrompt,
+    previousPrompt,
+    setPreviousPrompt,
+    showResult,
+    setShowResult,
+    loading,
+    setLoading,
+    resultData,
+    setResultData,
+    onSent,
+    newChat,
+  };
+
+  return (
+    <Context.Provider value={contextValue}>{props.children}</Context.Provider>
+  );
+};
